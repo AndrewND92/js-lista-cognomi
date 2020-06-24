f7e3c97f42d14255624965b46c06ccbcf7e226a7
Andrea <miranda.andrea.nd@gmail.com>
2020-06-24 16:01:37 +0200

    bozza del lavoro

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var cognome;
+var cognome = prompt("Scrivi il tuo cognome");
+
+var lista = ["Esposito", "Nullazzi", "Ferretti"];
+
+for (var i = 0; i < lista.length; i++) {
+  console.log(lista[i]);
+}
+
+lista.push(cognome);
+console.log(lista[i]);
